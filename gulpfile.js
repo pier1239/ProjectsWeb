31316af96ee4644462e3262d36ef37d1a9dd5144
Unknown <belleyjeanphilippe@gmail.com>
2018-08-18 12:01:19 -0400

    JP Cleaned PL code

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,39 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 // Translate SASS to CSS
-gulp.task('sass', function(){
+gulp.task('sass', function() {
   return gulp.src('src/scss/style.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass   
-    .pipe(gulp.dest('src/scss/css'))
-  
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('default', function(){
- 
-gulp.watch('src/scss/style.scss', ['sass']);
-  
+// Html destination
+gulp.task('html', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('app/'));
+});
+
+// Images compression
+gulp.task('img', function() {
+  return gulp.src('src/images/*')
+    .pipe(gulp.dest('app/img/'));
+});
+
+
+// Images compression
+gulp.task('scripts', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(gulp.dest('app/js/'));
+});
+
+// Default task
+gulp.task('default', ['sass', 'html', 'img', 'scripts'], function() {
+  console.log('******** Default task done ********');
+});
+
+// Gulp watch
+gulp.task('watch', ['sass', 'html', 'img', 'scripts'], function() {
+  console.log('******** Gulp is watching you ********');
+  gulp.watch('src/scss/*.scss', ['sass']);
+  gulp.watch('src/**/*.html', ['html']);
 });
