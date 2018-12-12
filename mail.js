bb2ba727dc2ad8b3fa815a4d7e1260efd667f57a
Pierre-Luc Doré <pier1239@hotmail.com>
2018-12-12 14:18:09 -0500

    test resolve issues

--- mail.js
+++ mail.js
@@ -1,40 +1,55 @@
-var express = require("express");
+//var express = require("express");
 
-var app = express();
-const bodyParser = require('body-parser');
-app.use(express.static(__dirname + '/app'));
-app.use(express.static(__dirname + '/css'));
-app.use(bodyParser.urlencoded({ extended: false }));
+//var app = express();
+//const bodyparser = require('body-parser');
+//app.use(express.static(__dirname + '/app'));
+//app.use(express.static(__dirname + '/css'));
+//app.use(bodyparser.urlencoded({ extended: false }));
 
-app.get('/',function(req,res){
+//app.get('/',function(req,res){
 
-});
+//});
 
-app.post('/',function(req,res){
+//app.post('/',function(req,res){
 
-require('env2')('sendgrid.env');    // loads all entries into process.env
+//require('env2')('sendgrid.env');    // loads all entries into process.env
 
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDMAIL_KEY);
-const msg = {
-to: 'piertest1239@gmail.com',
-from: req.body.email,
-subject:  req.body.subject,
-text: 'ces facil a prendre',
-html:   req.body.name + ' ' + req.body.message,
-};
-sgMail.send(msg);
+//const sgMail = require('@sendgrid/mail');
+//sgMail.setApiKey(process.env.SENDMAIL_KEY);
+//const msg = {
+//to: 'piertest1239@gmail.com',
+//from: req.body.email,
+//subject:  req.body.subject,
+//text: 'ces facil a prendre',
+//html:   req.body.name + ' ' + req.body.message,
+//};
+//sgMail.send(msg);
 
-res.redirect('/');
+//res.redirect('/');
     
 
   
 
 
 
-});
+//});
 
 
 
-app.listen(process.env.PORT || 5000);
+//app.listen(process.env.PORT || 5000);
+
+
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'app')))
+  .set('css', path.join(__dirname, 'css'))
+  
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
 
