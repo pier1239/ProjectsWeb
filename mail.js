c7ebe7c562e346ca64abe348edbb5e7021d655b8
Pierre-Luc Doré <pier1239@hotmail.com>
2018-12-17 15:26:43 -0500

    test

--- mail.js
+++ mail.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, '/app')))
-  .set('app', path.join(__dirname, 'app'))
+  .set('app', path.join(__dirname, '/app'))
 
 
  	
