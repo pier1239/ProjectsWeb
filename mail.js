a2c561f473e60b7515bc01f5cce283faa640bd99
Pierre-Luc Doré <pier1239@hotmail.com>
2018-12-17 10:45:48 -0500

    json lines added

--- mail.js
+++ mail.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/',function(req,res){
 
- res.sendFile(path.join(__dirname+'index.html'));
+ res.sendFile(path.join(__dirname+'/index.html'));
 
 
 });
