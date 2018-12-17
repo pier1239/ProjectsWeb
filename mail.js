9b689c7b0fc46710770c20f12b5a17817a98a84f
Pierre-Luc Doré <pier1239@hotmail.com>
2018-12-17 16:01:22 -0500

    test

--- mail.js
+++ mail.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 5000
 express()
   .use('/', express.static('app'))
 
-
-
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
  	
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
