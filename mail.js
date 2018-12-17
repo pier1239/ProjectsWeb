8a2ca15d283ce5e71223ede7ed95c6fbe47820d7
Pierre-Luc Doré <pier1239@hotmail.com>
2018-12-17 11:10:08 -0500

    appuse

--- mail.js
+++ mail.js
@@ -3,14 +3,15 @@ const ejs = require("ejs")
 var app = express();
 var path    = require("path");
 const bodyParser = require('body-parser');
-app.use(express.static(__dirname + '/app'));
+app.use(express.static('app'));
+//app.use(express.static(__dirname + '/app'));
 //app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/css'));
+//app.use(express.static(__dirname + '/css'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/',function(req,res){
-	 res.sendFile(path.join(__dirname+'/css/style.css'));
+	
 	
 
  res.sendFile(path.join(__dirname+'/index.html'));
