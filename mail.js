f3a55550ed93ff0788f7881bfea5d0b5d17d6427
Pierre-Luc Doré <pier1239@hotmail.com>
2018-12-18 12:54:21 -0500

    eja test

--- mail.js
+++ mail.js
@@ -29,24 +29,24 @@ res.render('index');
 
 app.post('/',function(req,res){
 
-require('env2')('sendgrid.env');    // loads all entries into process.env
+//require('env2')('sendgrid.env');    // loads all entries into process.env
 
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDMAIL_KEY);
-const msg = {
-to: 'piertest1239@gmail.com',
-from: req.body.email,
-subject:  req.body.subject,
-text: 'ces facil a prendre',
-html:   req.body.name + ' ' + req.body.message,
-};
-sgMail.send(msg);
+//const sgMail = require('@sendgrid/mail');
+//sgMail.setApiKey(process.env.SENDMAIL_KEY);
+//const msg = {
+//to: 'piertest1239@gmail.com',
+//from: req.body.email,
+//subject:  req.body.subject,
+//text: 'ces facil a prendre',
+//html:   req.body.name + ' ' + req.body.message,
+//};
+//sgMail.send(msg);
 
-res.redirect('/');
+//res.redirect('/');
     
 
   
-
+res.end("ton port ne marche pas coliss");
 
 
 });
