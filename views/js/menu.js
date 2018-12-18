eab31bc19d025967af6b80a60b0ee3b36aa73c89
Pierre-Luc Doré <pier1239@hotmail.com>
2018-12-18 11:36:44 -0500

    eja test

---
+++ views/js/menu.js
@@ -0,0 +1,22 @@
+
+
+$( document ).ready(function() {
+$('.navigation-btn').click(function(){
+  $('.navigation').toggleClass('isopen');
+
+
+
+
+});
+
+$( '.navigation a' ).on("click", function(){
+ $('.navigation').toggleClass('isopen');
+
+});
+
+});
+
+
+
+
+
